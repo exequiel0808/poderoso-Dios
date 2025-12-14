2a61bffbfe0c7bb19db139173fe94bcca1220790
exequiel0808 <elsalvador.00elrey@gmail.com>
2025-12-13 21:11:44 -0600

    Add files via upload

---
+++ js/efectos.js
@@ -0,0 +1,10 @@
+// -------------------------------------------
+// EFECTOS EXTRA
+// Aquí puedes agregar animaciones, fade-in,
+// efectos de luz, transiciones, etc.
+// -------------------------------------------
+
+window.addEventListener("scroll", () => {
+    console.log("Scrolling detectado");
+});
+
